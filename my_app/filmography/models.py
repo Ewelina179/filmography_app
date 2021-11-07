4de0720b8b8301478296e0f68b8502a3207ec02c
Ewelina179 <ewelinadalek@gmail.com>
2021-11-07 13:46:08 +0100

    added constraint

--- my_app/filmography/models.py
+++ my_app/filmography/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Movie(models.Model):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128, unique=True)
 
 class Actor(models.Model):
     fullname = models.CharField(max_length=128)
-    movies = models.ManyToManyField(Movie)
+    movies = models.ManyToManyField(Movie) # musi być coś więcej, żeby odróżnić ludzi o tym samym fullname
 
 class User(AbstractUser):
     pass
-    actors = models.ManyToManyField(Actor, through='Favourite')
+    actors = models.ManyToManyField(Actor, through='Favourite') # tu unique? Bo się dodaje w nieskonczoność
 
 class Favourite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
-    is_favourite = models.BooleanField()
+    is_favourite = models.BooleanField() # domyślnie musi być False
