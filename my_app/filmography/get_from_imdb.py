917b04abcf4e9f74f34f94d658dc402c9d823a29
Ewelina179 <ewelinadalek@gmail.com>
2021-11-08 15:18:23 +0100

    changed get_actors_ids

--- my_app/filmography/get_from_imdb.py
+++ my_app/filmography/get_from_imdb.py
@@ -32,26 +32,33 @@ class ActorInfo:
         content = response.json()
         return content
 
-    def get_actor_id(self, data):
-        actor_id = self.get_data("id", data)
-        id = actor_id["d"][0]["id"]
-        return id
+    def get_actor_id(self, fullname):
+        data = []
+        actor_id = self.get_data("id", fullname)
+        id = actor_id["d"]
+        for el in id:
+            if el.get("i"):
+                actor = {}
+                actor = {"name": el["l"], "id": el["id"], "image": el['i']["imageUrl"]}
+                data.append(actor)
+        return data
+        # ALGORYTM LEVEINSHTEINA ALBO COŚ W TEN DESEN JUŻ KTOŚ INNY ZAIMPLEMENTOWAŁ DO TEGO ;p
+        # zwraca listę słowników z name, id i image poszczególnych aktorów
 
-    def get_actor_info(self, data):
-        info = self.get_data("actor_data", self.get_actor_id(data)) # tu warunkować! jeśli id w bazie to z bazy, żeby nie robić niepotrzebnie requestu
+    def get_actor_info(self, id):
+        info = self.get_data("actor_data", id)
         return info
 
-    def get_actor_filmography(self, data):
-        filmography = self.get_actor_info(data)["filmography"]
-        #print(self.get_actor_info(data)["filmography"])
+    def get_actor_filmography(self,id):
+        filmography = self.get_actor_info(id)["filmography"]
         lst_of_filmography = []
         for el in filmography:
             if el["category"]=="actress" or el["category"]=="actor":
-                lst_of_filmography.append(el["title"]) # gdzie category: actress or actor
+                lst_of_filmography.append(el["title"])
         return lst_of_filmography
 
 actor = ActorInfo(os.getenv("API_KEY"))
-#x = actor.get_actor_filmography("Felicity Jones")
-#y = actor.get_actor_info("Felicity Jones")
+#x = actor.get_actor_filmography("nm0000148") # po id!
+y = actor.get_actor_id("Felicity Jones")
 #print(x)
-#print(y)
+print(y)
