72e1efb384e65e2996c8851f55d756fa5316eeb8
Ewelina179 <ewelinadalek@gmail.com>
2021-11-09 23:25:24 +0100

    added urls

--- my_app/filmography/urls.py
+++ my_app/filmography/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from filmography.views import home, dashboard
+from filmography.views import home, dashboard, actors, actor_detail, get_actor
 
 urlpatterns = [
     url(r"^home/", home, name="home"),
     url(r"^dashboard/", dashboard, name="dashboard"),
+    url(r"^actors", actors, name="actors"),
+    url(r"^actor/(?P<nm>\D+)", actor_detail, name="actor_detail"),
+    url(r"^search", get_actor, name="get_actor"),
 ]
