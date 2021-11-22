097a2ae21ab8f3cd928f1e89405a1ecfb686af72
Ewelina179 <ewelinadalek@gmail.com>
2021-11-22 22:23:47 +0100

    added signals

--- my_app/filmography/signals.py
+++ my_app/filmography/signals.py
@@ -1,4 +1,5 @@
-from .models import ActorUserRequest
+from django.contrib.auth.models import User
+from .models import ActorUserRequest, UserProfile
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -13,6 +14,11 @@ def get_from_api(sender, instance, created, **kwargs):
         instance.save()
         print(actors)
 
-#@receiver(post_save, sender=ActorUserRequest)
-#def update_actoruserrequest(sender, instance, **kwargs):
-#    pass
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        x = UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
