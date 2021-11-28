38e7190752e1098d6f0a255bef212beb9ff4b5a5
Ewelina179 <ewelinadalek@gmail.com>
2021-11-28 09:56:21 +0100

    added model movierequest and managers

--- my_app/filmography/models.py
+++ my_app/filmography/models.py
@@ -1,25 +1,55 @@
+from django.apps import apps
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+
+from .get_from_imdb import ActorInfo
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=64, null=True)
     last_name = models.CharField(max_length=64, null=True)
     avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
-    objects = models.Manager()
 
 class ActorUser(models.Model):
     user = models.ForeignKey('filmography.UserProfile', on_delete=models.CASCADE)
     actor = models.ForeignKey('filmography.Actor', on_delete=models.CASCADE)
 
 
+class ActorManager(models.Manager):
+
+    def register_from_response(self, response):
+        for entry in response:
+            if 'nm' in entry['id']:
+                self.get_or_create(
+                    imdb_id=entry['id'],
+                    defaults={
+                        'fullname': entry['name']
+                    }
+                )
+    
 class Actor(models.Model):
     fullname = models.CharField(max_length=128)
+    imdb_id = models.CharField(max_length=64, unique=True)
+
+    objects = ActorManager()
+
+class MovieManager(models.Manager):
+    def register_from_response(self, response):
+        for entry in response:
+            self.get_or_create(
+                id_from_imdb = entry['id'],
+                defaults={
+                        'title': entry['title']
+                    }
+            )
 
 
 class Movie(models.Model):
     id_from_imdb = models.CharField(max_length=128, unique=True)
     title = models.CharField(max_length=128)
+
+    objects = MovieManager()
 
 
 class ActorMovie(models.Model):
@@ -32,3 +62,46 @@ class ActorUserRequest(models.Model):
     datetime = models.DateTimeField(auto_now_add=True)
     phrase = models.CharField(max_length=64)
     response = models.TextField(blank=True, default="")
+    status = models.CharField(
+        max_length=1,
+        choices=(('p', 'W kolejce'), ('r', 'W trakcie pobierania'), ('e', 'Błąd'), ('d', 'Pobrano')), default='p'
+    )
+
+    def set_response(self):
+        self.status = 'r'
+        self.save()
+        try:
+            actor = ActorInfo(settings.API_KEY)
+            actors = actor.get_actors_ids(self.phrase)
+        except:
+            self.status = 'e'
+            self.save()
+        else:
+            self.response = actors
+            self.status = 'd'
+            self.save()
+            apps.get_model('filmography.Actor').objects.register_from_response(self.response)
+
+class MovieRequest(models.Model):
+    actor_imdb_id = models.CharField(max_length=64)
+    datetime = models.DateTimeField(auto_now_add=True)
+    response = models.TextField(blank=True, default="")
+    status = models.CharField(
+        max_length=1,
+        choices=(('p', 'W kolejce'), ('r', 'W trakcie pobierania'), ('e', 'Błąd'), ('d', 'Pobrano')), default='p'
+    )
+
+    def set_response(self):
+        self.status = 'r'
+        self.save()
+        try:
+            actor = ActorInfo(settings.API_KEY)
+            movies = actor.get_actor_filmography(self.actor_imdb_id)
+        except:
+            self.status = 'e'
+            self.save()
+        else:
+            self.response = movies
+            self.status = 'd'
+            self.save()
+            x = apps.get_model('filmography.Movie').objects.register_from_response(self.response)
--- my_app/filmography/signals.py
+++ my_app/filmography/signals.py
@@ -1,18 +1,9 @@
 from django.contrib.auth.models import User
-from .models import ActorUserRequest, UserProfile
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-from .get_from_imdb import actor
-
-
-@receiver(post_save, sender=ActorUserRequest)
-def get_from_api(sender, instance, created, **kwargs):
-    if created:
-        actors = actor.get_actors_ids(instance.phrase)
-        instance.response = actors
-        instance.save()
-        print(actors)
+from .models import Actor, Movie, UserProfile
+from django.apps import apps
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -22,3 +13,8 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+@receiver(post_save, sender=Actor)
+def get_movies_from_api(sender, instance, created, **kwargs):
+    if created:
+        movies = apps.get_model("filmography.MovieRequest").objects.create(actor_imdb_id=instance.imdb_id)
