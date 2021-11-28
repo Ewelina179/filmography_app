1ed8a21909756724940d7e9d6b81bff928f0c2b9
Ewelina179 <ewelinadalek@gmail.com>
2021-11-28 10:00:51 +0100

    added commands get_data get_movie_data

---
+++ my_app/filmography/management/commands/get_movie_data.py
@@ -0,0 +1,10 @@
+from django.apps import apps
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        api_requests = apps.get_model("filmography.MovieRequest").objects.filter(status='p')
+        if api_requests.exists():
+            obj = api_requests.last()
+            obj.set_response()
