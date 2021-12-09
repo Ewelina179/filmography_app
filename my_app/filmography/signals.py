13a1938cdfb402199bdc65ea8c23389a1088a217
Ewelina179 <ewelinadalek@gmail.com>
2021-12-09 13:32:12 +0100

    added signal to change daily_api_counter

--- my_app/filmography/signals.py
+++ my_app/filmography/signals.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Actor, Movie, UserProfile
+from .models import Actor, ActorUserRequest, Movie, UserProfile
 from django.apps import apps
 
 from .get_from_imdb import ActorInfo
@@ -20,3 +20,13 @@ def save_profile(sender, instance, **kwargs):
 def get_movies_from_api(sender, instance, created, **kwargs):
     if created:
         movies = apps.get_model("filmography.MovieRequest").objects.create(actor_imdb_id=instance.imdb_id)
+
+
+@receiver(post_save, sender=ActorUserRequest) # chyba powinno być jak zmienia status?
+def change_daily_api_counter(sender, instance, created, **kwargs):
+    if created:
+        pass
+        #print(instance.phrase)
+        #print(instance.status)
+        #print(instance.user) # tutaj pusto (?)
+        #print(apps.get_model('filmography.UserProfile').objects.filter(user=instance.user.userprofile))
