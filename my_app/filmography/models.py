63e8a03c748ff99f6ff6b2c3d19e34f24b92de15
Ewelina179 <ewelinadalek@gmail.com>
2021-12-11 18:19:43 +0100

    changed methods set response

--- my_app/filmography/models.py
+++ my_app/filmography/models.py
@@ -27,7 +27,7 @@ class ActorUserManager(models.Manager):
 class ActorUser(models.Model):
     user = models.ForeignKey('filmography.UserProfile', on_delete=models.CASCADE)
     actor = models.ForeignKey('filmography.Actor', on_delete=models.CASCADE)
-    phrase = models.CharField(max_length=64)
+    phrase = models.CharField(max_length=64) # usunąć
 
     objects=ActorUserManager()
 
@@ -81,15 +81,6 @@ class ActorMovie(models.Model):
 
     objects = ActorMovieManager()
 
-class ActorUserRequestManager(models.Manager):
-    def check_is_in_db(self, response):
-        lst_of_actors = []
-        for entry in response:
-            if self.get(actor = Actor.objects.get(imdb_id=entry['id'])):
-                lst_of_actors.append(entry)
-        return lst_of_actors
-# szukaj po frazie id aktora w bazie aktor id imdb jak są pasujący to zrób ich listę ze słowników jak w response żeby wypluło to samo co do register from response actoruser, zeby tam zapisało tego aktora
-
 class ActorUserRequest(models.Model):
     user = models.ForeignKey('filmography.UserProfile', on_delete=models.SET_NULL, null=True)
     datetime = models.DateTimeField(auto_now_add=True)
@@ -100,33 +91,33 @@ class ActorUserRequest(models.Model):
         choices=(('p', 'W kolejce'), ('r', 'W trakcie pobierania'), ('e', 'Błąd'), ('d', 'Pobrano')), default='p'
     )
 
-    objects = ActorUserRequestManager()
-
     def set_response(self):
         self.status = 'r'
         self.save()
-        try: # nie wiem jak to wyciągnąć po frazie. z manadżerem? atrybut fraza w modelu aktor to raczej nie jest dobry pomysł
-            try:
-                self.objects.check_is_in_db(self.phrase)
-                apps.get_model('filmography.ActorUser').objects.register_from_response(self.objects.check_is_in_db(self.phrase), self.user, self.phrase)
-                print("Coś")
-                self.status = 'd'
-            except:
-                actor = ActorInfo(settings.API_KEY)
-                actors = actor.get_actors_ids(self.phrase)
-        except:
-            self.status = 'e'
-            self.save()
-        else:
-            self.response = actors
+        print(self.phrase)
+        x=Actor.objects.filter(fullname__icontains=self.phrase)
+        if x:
+            self.response="Aktor jest w bazie."
             self.status = 'd'
             self.save()
-            apps.get_model('filmography.Actor').objects.register_from_response(self.response)
-            apps.get_model('filmography.ActorUser').objects.register_from_response(self.response, self.user, self.phrase)
-            x = apps.get_model('filmography.UserProfile').objects.filter(user=self.user.user).first()
-            x.daily_api_counter +=1
-            x.save()
-
+            print(self.response)
+            print(x)
+            print(self.status)
+        else:
+            try:
+                actor = ActorInfo(settings.API_KEY)
+                actors = actor.get_actors_ids(self.phrase)
+            except:
+                self.status = 'e'
+                self.save()
+            else:
+                self.response = actors
+                self.status = 'd'
+                self.save()
+                apps.get_model('filmography.Actor').objects.register_from_response(self.response)
+                x = apps.get_model('filmography.UserProfile').objects.filter(user=self.user.user).first()
+                x.daily_api_counter +=1
+                x.save()
 
 class MovieRequest(models.Model):
     actor_imdb_id = models.CharField(max_length=64)
@@ -140,16 +131,22 @@ class MovieRequest(models.Model):
     def set_response(self):
         self.status = 'r'
         self.save()
-        try:
-            # duo uzupełnienia kiedy ActorUserRequets będzie poprawnie. sprawdż czy jest w bazie. jeśli tak, status d. jeśli nie dopiero to co niżej
-            actor = ActorInfo(settings.API_KEY)
-            movies = actor.get_actor_filmography(self.actor_imdb_id)
-        except:
-            self.status = 'e'
-            self.save()
-        else:
-            self.response = movies
+        x = Actor.objects.filter(imdb_id=self.actor_imdb_id).first() # Czy jest to konieczne, czy zbędne.
+        y = ActorMovie.objects.filter(actor=x.id)
+        if y:
+            self.response="Filmy znajdują się w bazie."
             self.status = 'd'
             self.save()
-            apps.get_model('filmography.Movie').objects.register_from_response(self.response)
-            apps.get_model('filmography.ActorMovie').objects.register_from_response(self.response, self.actor_imdb_id)
+        else:
+            try:
+                actor = ActorInfo(settings.API_KEY)
+                movies = actor.get_actor_filmography(self.actor_imdb_id)
+            except:
+                self.status = 'e'
+                self.save()
+            else:
+                self.response = movies
+                self.status = 'd'
+                self.save()
+                apps.get_model('filmography.Movie').objects.register_from_response(self.response)
+                apps.get_model('filmography.ActorMovie').objects.register_from_response(self.response, self.actor_imdb_id)
