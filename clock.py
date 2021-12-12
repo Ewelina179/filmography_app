fb738f017f88f7a75f467c5fb819559805de2df7
Ewelina179 <ewelinadalek@gmail.com>
2021-12-12 12:08:06 +0100

    test clock

--- clock.py
+++ clock.py
@@ -3,13 +3,17 @@ from .my_app.filmography.models import ActorUserRequest
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=1) #chyba to nie powinno być tutaj
 def timed_job():
     api_requests = ActorUserRequest.objects.filter(status='p')
     if api_requests.exists():
         obj = api_requests.last()
         obj.set_response()
     print('This job is run every one minute.')
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('This job is run every three minutes.')
 
 
 sched.start()
