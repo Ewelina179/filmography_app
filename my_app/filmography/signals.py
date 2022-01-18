a7bf1a436d7b3fa7c7cfb0a9aa48d14adf473df0
Ewelina179 <ewelinadalek@gmail.com>
2022-01-18 13:03:34 +0100

    added signals post_save actoruserrequest and movierequest

--- my_app/filmography/signals.py
+++ my_app/filmography/signals.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Actor, ActorUserRequest, Movie, UserProfile
+from .models import Actor, ActorUserRequest, MovieRequest, UserProfile
+from .forms import ActorUserRequestForm
 from django.apps import apps
 
 from .get_from_imdb import ActorInfo
@@ -21,12 +22,17 @@ def get_movies_from_api(sender, instance, created, **kwargs):
     if created:
         movies = apps.get_model("filmography.MovieRequest").objects.create(actor_imdb_id=instance.imdb_id)
 
+@receiver(post_save, sender=ActorUserRequest)
+def get_actors_from_api(sender, instance, created, **kwargs):
+    if created:
+        actouserrequest = apps.get_model("filmography.ActorUserRequest").set_response(instance)
 
-@receiver(post_save, sender=ActorUserRequest) # chyba powinno być jak zmienia status?
+@receiver(post_save, sender=MovieRequest)
+def get_actors_from_api(sender, instance, created, **kwargs):
+    if created:
+        movierequest = apps.get_model("filmography.MovieRequest").set_response(instance)
+
+@receiver(post_save, sender=ActorUserRequest)
 def change_daily_api_counter(sender, instance, created, **kwargs):
     if created:
         pass
-        #print(instance.phrase)
-        #print(instance.status)
-        #print(instance.user) # tutaj pusto (?)
-        #print(apps.get_model('filmography.UserProfile').objects.filter(user=instance.user.userprofile))
