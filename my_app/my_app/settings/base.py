a8bc7efdea79d03823f6c6fb5548a2d4ad4ad15a
Ewelina179 <ewelinadalek@gmail.com>
2022-01-24 13:16:48 +0100

    changed config pytest

--- my_app/my_app/settings/base.py
+++ my_app/my_app/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_app.settings")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
