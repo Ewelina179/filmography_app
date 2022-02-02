e28594cf40100dcd12072ad3708ab4a5a3995aae
Ewelina179 <ewelinadalek@gmail.com>
2022-02-02 10:58:11 +0100

    added tests

--- my_app/filmography/tests/tests_model.py
+++ my_app/filmography/tests/tests_model.py
@@ -1,41 +1,66 @@
 from django.contrib.auth.models import User
-from filmography.models import Actor, UserProfile
+from filmography.models import Actor, UserProfile, Movie, ActorMovie
+from filmography.models import ActorUser
 
 import pytest
 
-@pytest.mark.django_db
-def test_new_user(user_factory):
-    user = user_factory.create()
-    count = User.objects.all().count()
+#@pytest.mark.django_db
+#def test_new_user(user_factory):
+#    user = user_factory.create()
+#    count = User.objects.all().count()
     #print(user.username)
     #print(count)
-    assert True
+#    assert True
 
 @pytest.fixture
-def test_user():
+def user():
     return User.objects.create_user('ann', 'lennon@thebeatles.com', 'annpassword')
+
+#@pytest.fixture
+#def userprofile(user):
+#    return UserProfile.objects.create(user=user, first_name="Jan", last_name="Kowalski")
 
 #@pytest.mark.django_db
 #def test_user_create():
 #   User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
 #    assert User.objects.count() == 1
 
-#@pytest.mark.django_db
-#def test_db_user(test_user):
-#    userprofile = UserProfile.objects.create(user = test_user)
-#    assert UserProfile.user == test_user
-
-
 @pytest.fixture
-def test_actor():
+def actor():
     return Actor.objects.create(fullname = "Natalie Portman", imdb_id = "nm0000204")
 
-@pytest.mark.django_db(True)
-def test_actor_in_db(test_actor):
+@pytest.mark.django_db
+def test_actor_in_db(actor):
     actor = Actor.objects.get(fullname = "Natalie Portman")
-    assert actor == test_actor 
+    assert actor == actor 
 
 #@pytest.mark.django_db(True)
 #def test_actor_in_db_2(test_actor):
 #    actor = Actor.objects.get(fullname = "Natalie P")
 #    assert actor == test_actor
+
+@pytest.fixture
+def actoruser(actor, user):
+    return ActorUser.objects.create(userprofile=user.userprofile, actor=actor)
+
+@pytest.mark.django_db
+def test_actor_user_in_db(actor, user):
+    return ActorUser.objects.filter(actor=actor, user=user.userprofile).exists()
+
+@pytest.mark.django_db
+def test_userprofile(user):
+    user.userprofile.first_name = "Anna"
+    user.save()
+    assert user.userprofile.first_name == "Anna"
+
+@pytest.fixture
+def movie():
+    return Movie.objects.create(id_from_imdb="nm12345", title="ABC")
+
+@pytest.fixture
+def actormovie(actor, movie):
+    return ActorMovie.objects.create(actor=actor, movie=movie)
+
+@pytest.mark.django_db
+def test_actor_movie_in_db(actor, movie):
+    return ActorMovie.objects.filter(actor=actor, movie=movie).exists()
